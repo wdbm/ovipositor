402cb9e54472a06ffc3b3bdcd9ff97bf47364f29
Will Breaden Madden <wbm@protonmail.ch>
2018-06-19 21:25:03 +0100

    tidy

--- ovipositor/__init__.py
+++ ovipositor/__init__.py
@@ -73,7 +73,7 @@ import shijian
 import technicolor
 
 name        = "ovipositor"
-__version__ = "2018-06-19T2017Z"
+__version__ = "2018-06-19T2023Z"
 
 log = logging.getLogger(name)
 log.addHandler(technicolor.ColorisingStreamHandler())
@@ -102,6 +102,7 @@ def main(options = docopt.docopt(__doc__)):
   restart_regularly =       options["--restart_regularly"]
   interval_restart  = float(options["--restart_interval"])
   printout          =       options["--print_database_shortlinks"]
+  log.info("ovipositor")
   if printout:
     print_database_shortlinks()
     sys.exit()
--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 while true; do
-    sudo python3 ovipositor.py                               \
+    sudo python3 ovipositor/__init__.py                      \
         --socket=80                                          \
         --restart_regularly                                  \
         --restart_interval=300                               \
