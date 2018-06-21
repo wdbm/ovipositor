07f005d0477810c2c251f3257d73c8b2b4ab11c1
Will Breaden Madden <wbm@protonmail.ch>
2018-06-21 18:09:38 +0100

    tidy

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 while true; do
-    sudo python3 ovipositor/__init__.py                      \
-        --socket=80                                          \
-        --restart_regularly                                  \
-        --restart_interval=300                               \
+    ovipositor                 \
+        --socket=80            \
+        --restart_regularly    \
+        --restart_interval=300 \
         --home="index.html"
 done
